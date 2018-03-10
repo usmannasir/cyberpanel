551f41b3dcd4cde7e91bea7169e4e079dbe6af6c
qtwrk <qtwrk555@gmail.com>
2018-03-10 23:21:22 +0100

    Update install-cn.sh

--- install-cn.sh
+++ install-cn.sh
@@ -6,7 +6,7 @@ setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 wget https://mirror.cyberpanel.net/install-cn.tar.gz
 tar xzvf install-cn.tar.gz
-cd install
+cd install-cn
 chmod +x install.py
 server_ip="$(wget -qO- http://whatismyip.akamai.com/)"
 python install.py $server_ip
