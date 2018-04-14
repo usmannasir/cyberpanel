e984d9e01f98fea64026586a01bae0c11105ade0
qtwrk <qtwrk555@gmail.com>
2018-04-14 18:41:00 +0200

    check if apache is running before installation
    check if apache is running before installation

--- install.sh
+++ install.sh
@@ -4,12 +4,20 @@ rm -f /etc/yum.repos.d/epel.repo
 rm -f /etc/yum.repos.d/epel.repo.rpmsave
 yum install epel-release -y
 #some provider's centos7 template come with incorrect or misconfigured epel.repo
+if systemctl is-active httpd | grep -q 'active'; then
+  systemctl stop httpd
+  systemctl disable httpd
+  echo "Disabling apache to avoid OpenLiteSpeed conflicts..."
+  else
+  echo "Apache is not install or active, to next step..."
+fi
+# above if will check if server has apache running that occupies port 80/443 which conflicts with OLS
 if systemctl is-active named | grep -q 'active'; then
   systemctl stop named
   systemctl disable named
-  echo "Disabling named to aviod powerdns conflicts..."
+  echo "Disabling named to avoid powerdns conflicts..."
   else
-  echo "named is not installed or active, to next step..."
+  echo "Named is not installed or active, to next step..."
 fi
 # above if will check if server has named.service running that occupies port 53 which makes powerdns failed to start
 yum clean all
