a3ec139254089ad4707b1e7b22ea434cead378f4
Usman Nasir <usman@cyberpersons.com>
2019-10-02 16:08:31 +0500

    incremental backups: stage 4: job structure

--- IncBackups/models.py
+++ IncBackups/models.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from websiteFunctions.models import Websites
+from datetime import datetime
 
-# Create your models here.
+class IncJob(models.Model):
+    website = models.ForeignKey(Websites)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+
+class JobSnapshots(models.Model):
+    job = models.ForeignKey(IncJob)
+    type = models.CharField(max_length=50)
+    snapshotid = models.CharField(max_length=50)
--- IncBackups/views.py
+++ IncBackups/views.py
@@ -8,6 +8,13 @@ from plogical.processUtilities import ProcessUtilities
 from plogical.virtualHostUtilities import virtualHostUtilities
 import json
 import os
+from loginSystem.models import Administrator
+from websiteFunctions.models import Websites
+from .models import IncJob, JobSnapshots
+from .IncBackups import IncJobs
+from random import randint
+import time
+from plogical.CyberCPLogFileWriter import CyberCPLogFileWriter as logging
 # Create your views here.
 
 
@@ -23,7 +30,19 @@ def createBackup(request):
             return ACLManager.loadError()
 
         websitesName = ACLManager.findAllSites(currentACL, userID)
-        return defRenderer(request, 'IncBackups/createBackup.html', {'websiteList': websitesName})
+
+        destinations = []
+        destinations.append('local')
+
+        path = '/home/cyberpanel/sftp'
+
+        for items in os.listdir(path):
+            destinations.append('sftp:%s' % (items))
+
+        for items in os.listdir(path):
+            destinations.append('s3:s3.amazonaws.com/%s' % (items))
+
+        return defRenderer(request, 'IncBackups/createBackup.html', {'websiteList': websitesName, 'destinations': destinations})
     except BaseException, msg:
         return HttpResponse(str(msg))
 
@@ -234,6 +253,194 @@ def removeDestination(request):
 
 
         os.remove(ipFile)
+
+        final_dic = {'status': 1, 'error_message': 'None'}
+        final_json = json.dumps(final_dic)
+        return HttpResponse(final_json)
+
+    except BaseException, msg:
+        final_dic = {'destStatus': 0, 'error_message': str(msg)}
+        final_json = json.dumps(final_dic)
+        return HttpResponse(final_json)
+
+def fetchCurrentBackups(request):
+    try:
+        userID = request.session['userID']
+        currentACL = ACLManager.loadedACL(userID)
+        admin = Administrator.objects.get(pk=userID)
+
+        data = json.loads(request.body)
+        backupDomain = data['websiteToBeBacked']
+
+        if ACLManager.checkOwnership(backupDomain, admin, currentACL) == 1:
+            pass
+        else:
+            return ACLManager.loadErrorJson('fetchStatus', 0)
+
+        if ACLManager.checkOwnership(backupDomain, admin, currentACL) == 1:
+            pass
+        else:
+            return ACLManager.loadErrorJson()
+
+        website = Websites.objects.get(domain=backupDomain)
+
+        backups = website.incjob_set.all()
+
+        json_data = "["
+        checker = 0
+
+        for items in backups:
+
+            includes = ""
+
+            jobs = items.jobsnapshots_set.all()
+
+            for job in jobs:
+                includes = '%s,%s:%s' % (includes, job.type, job.snapshotid)
+
+            dic = {'id': items.id,
+                   'date': str(items.date),
+                   'includes': includes
+                   }
+
+            if checker == 0:
+                json_data = json_data + json.dumps(dic)
+                checker = 1
+            else:
+                json_data = json_data + ',' + json.dumps(dic)
+
+        json_data = json_data + ']'
+        final_json = json.dumps({'status': 1, 'error_message': "None", "data": json_data})
+        return HttpResponse(final_json)
+    except BaseException, msg:
+        final_dic = {'status': 0, 'error_message': str(msg)}
+        final_json = json.dumps(final_dic)
+        return HttpResponse(final_json)
+
+def submitBackupCreation(request):
+    try:
+        userID = request.session['userID']
+        currentACL = ACLManager.loadedACL(userID)
+        admin = Administrator.objects.get(pk=userID)
+
+        data = json.loads(request.body)
+        backupDomain = data['websiteToBeBacked']
+        backupDestinations = data['backupDestinations']
+
+        if ACLManager.checkOwnership(backupDomain, admin, currentACL) == 1:
+            pass
+        else:
+            return ACLManager.loadErrorJson('metaStatus', 0)
+
+        tempPath = "/home/cyberpanel/" + str(randint(1000, 9999))
+
+        try:
+            websiteData = data['websiteData']
+        except:
+            websiteData = False
+
+        try:
+            websiteEmails = data['websiteEmails']
+        except:
+            websiteEmails = False
+
+        try:
+            websiteSSLs = data['websiteSSLs']
+        except:
+            websiteSSLs = False
+
+        extraArgs = {}
+        extraArgs['website'] = backupDomain
+        extraArgs['tempPath'] = tempPath
+        extraArgs['backupDestinations'] = backupDestinations
+        extraArgs['websiteData'] = websiteData
+        extraArgs['websiteEmails'] = websiteEmails
+        extraArgs['websiteSSLs'] = websiteSSLs
+
+        startJob = IncJobs('createBackup', extraArgs)
+        startJob.start()
+
+
+        time.sleep(2)
+
+        final_json = json.dumps({'status': 1,  'error_message': "None", 'tempPath': tempPath})
+        return HttpResponse(final_json)
+
+    except BaseException, msg:
+        logging.writeToFile(str(msg))
+        final_dic = {'status': 0, 'metaStatus': 0, 'error_message': str(msg)}
+        final_json = json.dumps(final_dic)
+        return HttpResponse(final_json)
+
+def getBackupStatus(request):
+    try:
+        userID = request.session['userID']
+        data = json.loads(request.body)
+
+        status = data['tempPath']
+        backupDomain = data['websiteToBeBacked']
+
+        domain = Websites.objects.get(domain=backupDomain)
+
+        ## file name read ends
+
+        if os.path.exists(status):
+            command = "sudo cat " + status
+            status = ProcessUtilities.outputExecutioner(command, 'cyberpanel')
+
+            if status.find("Completed") > -1:
+
+                ### Removing Files
+
+                command = 'sudo rm -f ' + status
+                ProcessUtilities.executioner(command, 'cyberpanel')
+
+                final_json = json.dumps(
+                    {'backupStatus': 1, 'error_message': "None", "status": status, "abort": 1})
+                return HttpResponse(final_json)
+
+            elif status.find("[5009]") > -1:
+                ## removing status file, so that backup can re-run
+                try:
+                    command = 'sudo rm -f ' + status
+                    ProcessUtilities.executioner(command, 'cyberpanel')
+
+                except:
+                    pass
+
+                final_json = json.dumps(
+                    {'backupStatus': 1, 'error_message': "None", "status": status,
+                     "abort": 1})
+                return HttpResponse(final_json)
+            else:
+                final_json = json.dumps(
+                    {'backupStatus': 1, 'error_message': "None", "status": status,
+                     "abort": 0})
+                return HttpResponse(final_json)
+        else:
+            final_json = json.dumps({'backupStatus': 1, 'error_message': "None", "status": 1, "abort": 0})
+            return HttpResponse(final_json)
+
+    except BaseException, msg:
+        final_dic = {'backupStatus': 0, 'error_message': str(msg)}
+        final_json = json.dumps(final_dic)
+        logging.writeToFile(str(msg) + " [backupStatus]")
+        return HttpResponse(final_json)
+
+
+def deleteBackup(request):
+    try:
+        userID = request.session['userID']
+        currentACL = ACLManager.loadedACL(userID)
+
+        if ACLManager.currentContextPermission(currentACL, 'addDeleteDestinations') == 0:
+            return ACLManager.loadErrorJson('destStatus', 0)
+
+        data = json.loads(request.body)
+
+        id = data['backupID']
+
+        IncJob.objects.get(id=id).delete()
 
         final_dic = {'status': 1, 'error_message': 'None'}
         final_json = json.dumps(final_dic)
