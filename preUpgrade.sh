291a946f16af867b7ba9c4b7867e2afe2fbcf756
Usman Nasir <usman@cyberpersons.com>
2019-10-19 13:32:32 +0500

    bug fix: upgrade process

--- preUpgrade.sh
+++ preUpgrade.sh
@@ -6,4 +6,6 @@ if [ ! -d "/usr/local/CyberPanel" ]; then
   pip install --ignore-installed -r requirments.txt
   virtualenv --system-site-packages /usr/local/CyberPanel
 fi
-
+rm -rf upgrade.py
+wget https://raw.githubusercontent.com/usmannasir/cyberpanel/1.8.0/plogical/upgrade.py
+/usr/local/CyberPanel/bin/python2 upgrade.py
