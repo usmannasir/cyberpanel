ef74af9313ab9b67c72e2ad81ce37bc22daf8efa
Usman Nasir <usman@cyberpersons.com>
2019-11-07 14:21:57 +0500

    update url to stable

--- preUpgrade.sh
+++ preUpgrade.sh
@@ -2,10 +2,10 @@ if [ ! -d "/usr/local/CyberPanel" ]; then
   virtualenv --system-site-packages /usr/local/CyberPanel
   source /usr/local/CyberPanel/bin/activate
   rm -rf requirments.txt
-  wget https://raw.githubusercontent.com/usmannasir/cyberpanel/1.8.0/requirments.txt
+  wget https://raw.githubusercontent.com/usmannasir/cyberpanel/stable/requirments.txt
   pip install --ignore-installed -r requirments.txt
   virtualenv --system-site-packages /usr/local/CyberPanel
 fi
 rm -rf upgrade.py
-wget https://raw.githubusercontent.com/usmannasir/cyberpanel/1.8.0/plogical/upgrade.py
+wget https://raw.githubusercontent.com/usmannasir/cyberpanel/stable/plogical/upgrade.py
 /usr/local/CyberPanel/bin/python2 upgrade.py
