6c178376d15ccf60d3204563830265861a976475
Usman Nasir <usman@cyberpersons.com>
2019-12-13 14:58:40 +0500

    vhu

--- plogical/virtualHostUtilities.py
+++ plogical/virtualHostUtilities.py
@@ -3,7 +3,9 @@ import os
 import os.path
 import sys
 import django
-
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 sys.path.append('/usr/local/CyberCP')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CyberCP.settings")
 
