fd8bb7f0f88142c71cd9360ad5f5b3f5593e8368
Usman Nasir <usman@cyberpersons.com>
2019-12-18 16:33:46 +0500

    bug fix: message length

--- emailMarketing/models.py
+++ emailMarketing/models.py
@@ -39,7 +39,7 @@ class EmailTemplate(models.Model):
     fromName = models.CharField(max_length=100)
     fromEmail = models.CharField(max_length=150)
     replyTo = models.CharField(max_length=150)
-    emailMessage = models.CharField(max_length=3000000)
+    emailMessage = models.CharField(max_length=65532)
 
 class EmailJobs(models.Model):
     owner = models.ForeignKey(EmailTemplate, on_delete=models.CASCADE)
