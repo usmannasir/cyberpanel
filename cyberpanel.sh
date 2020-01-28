b47a51b89b54a9ba1f4c1d25f3b9262f5ca6ac0a
qtwrk <qtwrk555@gmail.com>
2020-01-28 18:26:00 +0100

    fix lsphp74-zip

--- cyberpanel.sh
+++ cyberpanel.sh
@@ -1283,6 +1283,15 @@ watchdog_setup
 
 install_utility
 
+if [[ ! -f /usr/local/lsws/lsphp74/lib64/php/modules/zip.so ]] ; then
+	yum install -y http://packages.psychotic.ninja/7/plus/x86_64/RPMS/libzip-0.11.2-6.el7.psychotic.x86_64.rpm
+	yum install -y http://packages.psychotic.ninja/7/plus/x86_64/RPMS/libzip-devel-0.11.2-6.el7.psychotic.x86_64.rpm
+	/usr/local/lsws/lsphp74/bin/pecl install zip
+	echo "extension=zip.so" > /usr/local/lsws/lsphp74/etc/php.d/20-zip.ini
+fi
+#fix the lsphp74-zip missing issue.
+
+
 clear
 echo "###################################################################"
 echo "                CyberPanel Successfully Installed                  "
