e01c993d78dbeb15bf1a9bcb1bc6d4b487473515
Usman Nasir <usman@cyberpersons.com>
2020-01-29 14:25:42 +0500

    fix branch name

--- preUpgrade.sh
+++ preUpgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BRANCH_NAME=$(curl -s https://cyberpanel.net/version.txt | sed -e 's|{"version":"||g' -e 's|","build":|.|g'| sed 's:}*$::')
+BRANCH_NAME=v$(curl -s https://cyberpanel.net/version.txt | sed -e 's|{"version":"||g' -e 's|","build":|.|g'| sed 's:}*$::')
 
 rm -f /usr/local/cyberpanel_upgrade.sh
 wget -O /usr/local/cyberpanel_upgrade.sh https://raw.githubusercontent.com/usmannasir/cyberpanel/$BRANCH_NAME/cyberpanel_upgrade.sh 2>/dev/null
