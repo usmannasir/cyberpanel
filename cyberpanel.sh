5cd6e34ef03efa420e75bf0887ad781e4cfc6f2b
qtwrk <qtwrk555@gmail.com>
2020-01-30 19:16:32 +0100

    minor fix on lsphp74-zip

--- cyberpanel.sh
+++ cyberpanel.sh
@@ -1285,6 +1285,10 @@ watchdog_setup
 install_utility
 
 if [[ ! -f /usr/local/lsws/lsphp74/lib64/php/modules/zip.so ]] && [[ $SERVER_OS == "CentOS" ]] && [[ $CENTOS_8 == "False" ]]; then
+	yum list installed libzip-devel
+		if [[ $? == "0" ]] ; then
+			yum remove -y libzip-devel
+	fi
 	yum install -y http://packages.psychotic.ninja/7/plus/x86_64/RPMS/libzip-0.11.2-6.el7.psychotic.x86_64.rpm
 	yum install -y http://packages.psychotic.ninja/7/plus/x86_64/RPMS/libzip-devel-0.11.2-6.el7.psychotic.x86_64.rpm
 	/usr/local/lsws/lsphp74/bin/pecl install zip
--- cyberpanel_upgrade.sh
+++ cyberpanel_upgrade.sh
@@ -166,6 +166,10 @@ fi
 install_utility
 
 if [[ ! -f /usr/local/lsws/lsphp74/lib64/php/modules/zip.so ]] && [[ $SERVER_OS == "CentOS7" ]] ; then
+	yum list installed libzip-devel
+		if [[ $? == "0" ]] ; then
+			yum remove -y libzip-devel
+		fi
 	yum install -y http://packages.psychotic.ninja/7/plus/x86_64/RPMS/libzip-0.11.2-6.el7.psychotic.x86_64.rpm
 	yum install -y http://packages.psychotic.ninja/7/plus/x86_64/RPMS/libzip-devel-0.11.2-6.el7.psychotic.x86_64.rpm
 	/usr/local/lsws/lsphp74/bin/pecl install zip
