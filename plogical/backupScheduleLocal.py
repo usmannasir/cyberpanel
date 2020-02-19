9b553b5f369a2af99cb0e49f1aacc7f28fb9b6c9
Usman Nasir <usman@cyberpersons.com>
2020-02-19 17:42:45 +0500

    change domain verify regex

--- plogical/backupScheduleLocal.py
+++ plogical/backupScheduleLocal.py
@@ -39,7 +39,7 @@ class backupScheduleLocal:
             backupSchedule.remoteBackupLogging(backupLogPath, "")
 
             for virtualHost in os.listdir("/home"):
-                if match(r'([\da-z\.-]+\.[a-z\.]{2,12}|[\d\.]+)([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?', virtualHost, M | I):
+                if match(r'^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$', virtualHost, M | I):
                     try:
                         retValues = backupSchedule.createLocalBackup(virtualHost, backupLogPath)
 
