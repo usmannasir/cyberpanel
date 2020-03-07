6e5a9072cb33f9b346624ccd18f7b170b4045bcc
qtwrk <qtwrk555@gmail.com>
2020-03-07 23:38:38 +0100

    fix upgrade script

--- cyberpanel_upgrade.sh
+++ cyberpanel_upgrade.sh
@@ -177,7 +177,9 @@ rm -rf upgrade.py
 wget https://$GIT_CONTENT_URL/${BRANCH_NAME}/plogical/upgrade.py
 
 if [[ $SERVER_COUNTRY == "CN" ]] ; then
+sed -i 's|wget  https://raw.githubusercontent.com/usmannasir/cyberpanel/v1.9.4/lscpd-0.2.4 -P /usr/local/lscp/bin/|cp -f /usr/local/CyberCP/lscpd-0.2.4 /usr/local/lscp/bin/lscpd-0.2.4|g' upgrade.py
 sed -i 's|wget  https://raw.githubusercontent.com/usmannasir/cyberpanel/%s/lscpd-0.2.4 -P /usr/local/lscp/bin/|cp -f /usr/local/CyberCP/lscpd-0.2.4 /usr/local/lscp/bin/lscpd-0.2.4|g' upgrade.py
+sed -i $'s/0.2.4\' % (branch)/0.2.4\'/' upgrade.py
 sed -i 's|raw.githubusercontent.com/usmannasir/cyberpanel|'${GIT_CONTENT_URL}'|g' upgrade.py
 sed -i 's|git clone https://github.com/usmannasir/cyberpanel|git clone https://'${GIT_URL}'|g' upgrade.py
 fi
