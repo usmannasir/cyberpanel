af298646914a5f94f62907bceacb10315b874c8a
Usman Nasir <usman@cyberpersons.com>
2020-04-16 00:06:25 +0500

    bug fix: incremental backups path on ubuntu

--- install/install.py
+++ install/install.py
@@ -686,7 +686,6 @@ class preFlightsChecks:
                 preFlightsChecks.call(command, self.distro, command, command, 1, 0, os.EX_OSERR)
 
 
-
             self.stdOut("Install dovecot - do the install")
 
             if self.distro == centos:
