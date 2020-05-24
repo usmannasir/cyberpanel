0d8b3729b5616d7c2679d648d395281d4276de48
hennaboy <hennaboy@gmail.com>
2020-05-24 10:32:18 +0100

    Update mysqlUtilities.py
    centos 8

--- plogical/mysqlUtilities.py
+++ plogical/mysqlUtilities.py
@@ -413,7 +413,7 @@ password=%s
     def applyMySQLChanges(data):
         try:
 
-            if ProcessUtilities.decideDistro() == ProcessUtilities.centos:
+            if ProcessUtilities.decideDistro() == ProcessUtilities.centos or ProcessUtilities.decideDistro() == ProcessUtilities.cent8:
                 command = 'sudo mv /etc/my.cnf /etc/my.cnf.bak'
             else:
                 command = 'sudo mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bak'
@@ -430,7 +430,7 @@ password=%s
             writeToFile.close()
 
             ##
-            if ProcessUtilities.decideDistro() == ProcessUtilities.centos:
+            if ProcessUtilities.decideDistro() == ProcessUtilities.centos or ProcessUtilities.decideDistro() == ProcessUtilities.cent8:
                 command = 'sudo mv ' + tempPath + ' /etc/my.cnf'
             else:
                 command = 'sudo mv ' + tempPath + ' /etc/mysql/my.cnf'
@@ -440,7 +440,7 @@ password=%s
             return 1, None
 
         except BaseException as msg:
-            if ProcessUtilities.decideDistro() == ProcessUtilities.centos:
+            if ProcessUtilities.decideDistro() == ProcessUtilities.centos or ProcessUtilities.decideDistro() == ProcessUtilities.cent8:
                 command = 'sudo mv /etc/my.cnf.bak /etc/my.cnf'
             else:
                 command = 'sudo mv /etc/mysql/my.cnf.bak /etc/mysql//my.cnf'
