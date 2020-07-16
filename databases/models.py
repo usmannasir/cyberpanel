9e413648836e41ec08ea51f6caac4eb539f24d04
Usman Nasir <usman@cyberpersons.com>
2020-07-16 12:12:19 +0500

    add remote access dialog box

--- databases/models.py
+++ databases/models.py
@@ -10,3 +10,8 @@ class Databases(models.Model):
     website = models.ForeignKey(Websites, on_delete=models.CASCADE)
     dbName = models.CharField(max_length=50,unique=True)
     dbUser = models.CharField(max_length=50)
+
+class DBMeta(models.Model):
+    database = models.ForeignKey(Databases, on_delete=models.CASCADE)
+    key = models.CharField(max_length=200)
+    value = models.TextField()
