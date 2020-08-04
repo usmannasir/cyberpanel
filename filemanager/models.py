133eb86660b06adb6d18d4acc5b69302bdebfeae
Usman Nasir <usman@cyberpersons.com>
2020-08-04 20:48:53 +0500

    add trash record

--- filemanager/models.py
+++ filemanager/models.py
@@ -2,5 +2,11 @@
 
 
 from django.db import models
+from websiteFunctions.models import Websites
 
 # Create your models here.
+
+class Trash(models.Model):
+    website = models.ForeignKey(Websites,on_delete=models.CASCADE)
+    originalPath = models.CharField(max_length=500,default=None)
+    fileName = models.CharField(max_length=200, default=None)
