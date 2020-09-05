5d05de3604021d30cc3aa297fdd18a1d64dd2148
Usman Nasir <usman@cyberpersons.com>
2020-09-06 02:17:09 +0500

    generate token everytime phpmyadmin is requested

--- plogical/phpmyadminsignin.php
+++ plogical/phpmyadminsignin.php
@@ -52,9 +52,3 @@ else if(isset($_GET['password'])){
     header('Location: /dataBases/phpMyAdmin');
     return;
 }
-
-$params = session_get_cookie_params();
-setcookie(session_name(), '', time() - 86400, $params["path"], $params["domain"], $params["secure"], $params["httponly"] );
-session_destroy();
-header('Location: /dataBases/phpMyAdmin');
-return;
