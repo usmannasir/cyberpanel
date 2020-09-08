658792470e99e2058080fd496cc4780048fedd4d
Usman Nasir <usman@cyberpersons.com>
2020-09-08 20:43:58 +0500

    fix dovecot

--- install/install.py
+++ install/install.py
@@ -782,7 +782,7 @@ $cfg['Servers'][$i]['LogoutURL'] = 'phpmyadminsignin.php?logout';
             ##
 
             if self.distro == centos:
-                command = 'yum --enablerepo=gf-plus -y install dovecot dovecot-mysql'
+                command = 'yum --enablerepo=gf-plus -y install dovecot23 dovecot23-mysql'
             elif self.distro == cent8:
                 command = 'dnf install --enablerepo=gf-plus dovecot23 dovecot23-mysql -y'
             else:
