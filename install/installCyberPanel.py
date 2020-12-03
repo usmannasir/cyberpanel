78842d28a76312080956b91eb12def5515abefa0
Usman Nasir <usman@cyberpersons.com>
2020-12-03 10:58:34 +0500

    temp: bypass php8.0 fail on ubuntu

--- install/installCyberPanel.py
+++ install/installCyberPanel.py
@@ -199,7 +199,7 @@ class InstallCyberPanel:
 
             res = os.system(command)
             if res != 0:
-                InstallCyberPanel.stdOut("Failed to install PHP on Ubuntu.", 1, 0)
+                InstallCyberPanel.stdOut("Failed to install PHP on Ubuntu.", 0, 1)
 
         elif self.distro == centos:
             command = 'yum -y groupinstall lsphp-all'
