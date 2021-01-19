0903ee015fa898ef453ca4f64564964401555a57
Usman Nasir <usman@cyberpersons.com>
2021-01-19 15:27:04 +0500

    add progress to install

--- install/installLog.py
+++ install/installLog.py
@@ -13,7 +13,10 @@ class InstallLog:
         elif message.find('mount -o') > -1 or message.find('usermod -G lscpd,') > -1:
             message = '%s,%s' % (message.replace(',', '-'), InstallLog.currentPercent)
         else:
-             InstallLog.currentPercent = message.split(',')[1]
+            try:
+                InstallLog.currentPercent = message.split(',')[1]
+            except:
+                pass
 
         file = open(InstallLog.fileName,'a')
         file.writelines("[" + time.strftime(
