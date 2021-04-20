d880211f76909a940bc018b6e637732d861103dc
WhatTheServer <admin@whattheserver.com>
2021-04-20 18:30:27 -0400

    Update install.py

--- install/install.py
+++ install/install.py
@@ -56,6 +56,8 @@ def get_distro():
 
         if data.find('CentOS Linux release 8') > -1:
             return cent8
+        if data.find('AlmaLinux release 8') > -1:
+            return cent8
 
     else:
         logging.InstallLog.writeToFile("Can't find linux release file - fatal error")
