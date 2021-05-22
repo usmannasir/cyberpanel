a96a23a388473b80d8dd5d5ec7ffe1f49cebd8ff
Usman Nasir <usman@cyberpersons.com>
2021-05-22 17:52:11 +0500

    add debugging info

--- install/installCyberPanel.py
+++ install/installCyberPanel.py
@@ -93,18 +93,16 @@ class InstallCyberPanel:
                 command = 'tar zxf lsws-6.0-ent-x86_64-linux.tar.gz'
                 install.preFlightsChecks.call(command, self.distro, command, command, 1, 1, os.EX_OSERR)
 
-                print(self.serial)
-                print(os.getcwd())
-
-                time.sleep(10)
-
-                if self.serial.lower() == 'trial':
-                    command = 'wget -q --output-document=lsws-6.0/trial.key http://license.litespeedtech.com/reseller/trial.key'
+                if self.serial == '1111-2222-3333-4444':
+                    command = 'wget -q --output-document=/root/cyberpanel/install/lsws-6.0/trial.key http://license.litespeedtech.com/reseller/trial.key'
                     install.preFlightsChecks.call(command, self.distro, command, command, 1, 1, os.EX_OSERR)
                 else:
                     writeSerial = open('lsws-6.0/serial.no', 'w')
                     writeSerial.writelines(self.serial)
                     writeSerial.close()
+
+
+                time.sleep(50)
 
                 shutil.copy('litespeed/install.sh', 'lsws-6.0/')
                 shutil.copy('litespeed/functions.sh', 'lsws-6.0/')
