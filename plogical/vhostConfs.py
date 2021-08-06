f1a722a739250613b6c23b4ebc486f629205f205
istiak101 <30789544+istiak101@users.noreply.github.com>
2021-08-06 18:14:24 +0600

    Custom error page support OLS

--- plogical/vhostConfs.py
+++ plogical/vhostConfs.py
@@ -36,6 +36,18 @@ accesslog $VH_ROOT/logs/$VH_NAME.access_log {
   compressArchive         1
 }
 
+errorpage 403 {
+  url                     403.html
+}
+
+errorpage 404 {
+  url                     404.html
+}
+
+errorpage 500 {
+  url                     500.html
+}
+
 scripthandler  {
   add                     lsapi:{virtualHostUser} php
 }
@@ -109,6 +121,19 @@ accesslog $VH_ROOT/logs/{masterDomain}.access_log {
 phpIniOverride  {
 {open_basedir}
 }
+
+errorpage 403 {
+  url                     403.html
+}
+
+errorpage 404 {
+  url                     404.html
+}
+
+errorpage 500 {
+  url                     500.html
+}
+
 scripthandler  {
   add                     lsapi:{externalApp} php
 }
