56b8e4a9b6f5d1b52dbe5d70dfafa31cfa1b683a
Usman Nasir <usman@cyberpersons.com>
2021-11-06 23:56:51 +0500

    possible resolve of https://forums.cyberpanel.net/discussion/458/error-restore-website-5009

--- plogical/backupUtilities.py
+++ plogical/backupUtilities.py
@@ -890,7 +890,7 @@ class backupUtilities:
 
                 dbName = database.find('dbName').text
 
-                if VERSION == '2.1' and BUILD == '1':
+                if VERSION == '2.1' and int(BUILD) >= 1:
 
                     logging.CyberCPLogFileWriter.writeToFile('Backup version 2.1.1 detected..')
 
