eb02c110dd61d11bb13c8c4ae8289a217ffa8b1a
Alfin Auzikri <iyalfin@gmail.com>
2022-04-05 15:13:22 +0700

    Avoid blocking DNS at several ISPs in Indonesia

--- cyberpanel.sh
+++ cyberpanel.sh
@@ -21,6 +21,7 @@
 #Check_Panel() --->  check to make sure no other panel is installed
 #Check_Process() ---> check no other process like Apache is running
 #Check_Provider() ---> check the provider, certain provider like Alibaba or Tencent Yun may need some special change
+#Check_Organization() ---> check the organization, certain provider or organization like Telkom Indonesia or Biznet Networks Indonesia may need some special change
 #Check_Argument() ---> parse argument and go to Argument_Mode() or Interactive_Mode() respectively
 #Pre_Install_Setup_Repository() ---> setup/install repositories for centos system.
 #go to Pre_Install_Setup_CN_Repository() if server is within China.
@@ -65,6 +66,7 @@ Server_Country="Unknow"
 Server_OS=""
 Server_OS_Version=""
 Server_Provider='Undefined'
+Server_Organization='Undefined'
 
 Watchdog="On"
 Redis_Hosting="No"
@@ -395,6 +397,13 @@ else
   Server_Provider='Undefined'
 fi
 
+Check_Organization() {
+if [[ "$(curl --silent --max-time 10 -4 curl ipinfo.io/org)" = "AS17451 BIZNET NETWORKS" ]]; then
+  Server_Organization="Biznet Networks"
+else
+  Server_Organization="Undefined"
+fi
+
 if [[ -f /sys/devices/virtual/dmi/id/product_uuid ]]; then
   if [[ "$(cut -c 1-3 /sys/devices/virtual/dmi/id/product_uuid)" = 'EC2' ]] && [[ -d /home/ubuntu ]]; then
     Server_Provider='Amazon Web Service'
@@ -403,6 +412,7 @@ fi
 
 if [[ "$Debug" = "On" ]] ; then
   Debug_Log "Server_Provider" "$Server_Provider"
+  Debug_Log "Server_Organization" "$Server_Organization"
 fi
 }
 
@@ -1181,8 +1191,14 @@ if ! grep -q "pid_max" /etc/rc.local 2>/dev/null ; then
     echo -e "nameserver 100.100.2.136" > /etc/resolv.conf
     echo -e "nameserver 100.100.2.138" >> /etc/resolv.conf
   else
-    echo -e "nameserver 1.1.1.1" > /etc/resolv.conf
-    echo -e "nameserver 8.8.8.8" >> /etc/resolv.conf
+    # Avoid blocking DNS at several ISPs in Indonesia.
+    if [[ "$Server_Organization" = "Biznet Networks" ]] ; then
+      echo -e "nameserver 203.142.82.222" > /etc/resolv.conf
+      echo -e "nameserver 203.142.84.222" >> /etc/resolv.conf
+    else
+      echo -e "nameserver 1.1.1.1" > /etc/resolv.conf
+      echo -e "nameserver 8.8.8.8" >> /etc/resolv.conf
+    fi
   fi
 
   systemctl restart systemd-networkd >/dev/null 2>&1
@@ -1881,6 +1897,8 @@ Check_Panel
 Check_Process
 
 Check_Provider
+
+Check_Organization
 
 Check_Argument "$@"
 
