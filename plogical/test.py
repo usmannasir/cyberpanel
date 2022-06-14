61c79eb77947abdda8f970b8968c36a16bb2ace3
Usman Nasir <usman@cyberpersons.com>
2022-06-14 12:47:37 +0500

    local

--- plogical/test.py
+++
@@ -1,5 +0,0 @@
-import subprocess, shlex
-command = 'yum list | grep lsphp | xargs -n3 | column -t'
-result = subprocess.check_output(command, shell=True).splitlines()
-for item in result:
-    print(item.split(b' '))
