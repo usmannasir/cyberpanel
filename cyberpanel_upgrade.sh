0b3004cf92be5390d5a36e9ddee57abecedbf612
Usman Nasir <usman@cyberpersons.com>
2022-09-25 12:25:13 +0500

    bug fix: https://github.com/usmannasir/cyberpanel/issues/952: finally CP Running

--- cyberpanel_upgrade.sh
+++ cyberpanel_upgrade.sh
@@ -614,7 +614,7 @@ fi
 
 wget https://cyberpanel.sh/www.litespeedtech.com/packages/lsapi/wsgi-lsapi-2.1.tgz
 tar xf wsgi-lsapi-2.1.tgz
-cd wsgi-lsapi-2.1.tgz || exit
+cd wsgi-lsapi-2.1 || exit
 /usr/local/CyberPanel/bin/python ./configure.py
 make
 
