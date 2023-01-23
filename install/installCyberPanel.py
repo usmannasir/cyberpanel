5349f33aa250dbccdebd952268fb8811f3e7e252
usman@cyberpersons.com <usman@cyberpersons.com>
2023-01-23 20:47:07 +0500

    bug fx: php

--- install/installCyberPanel.py
+++ install/installCyberPanel.py
@@ -241,7 +241,7 @@ class InstallCyberPanel:
             command = 'dnf install lsphp71* lsphp72* lsphp73* lsphp74* lsphp80* --exclude lsphp73-pecl-zip --exclude *imagick* -y --skip-broken'
             subprocess.call(command, shell=True)
 
-            command = 'dnf install lsphp81* --exclude *imagick* -y'
+            command = 'dnf install lsphp81* --exclude *imagick* -y --skip-broken'
             subprocess.call(command, shell=True)
         
         if self.distro == openeuler:
