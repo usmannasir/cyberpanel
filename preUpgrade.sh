ebe92f7060a1ab0d6cc502280094c1027ac8718c
Nilberto César <nilbertocs@gmail.com>
2023-03-04 07:25:03 -0300

    Revert "Update preUpgrade.sh"
    This reverts commit 6899ba169fbd817f3525df952c8a2a85244dc2ac.

--- preUpgrade.sh
+++ preUpgrade.sh
@@ -3,6 +3,6 @@
 BRANCH_NAME=v$(curl -s https://cyberpanel.net/version.txt | sed -e 's|{"version":"||g' -e 's|","build":|.|g'| sed 's:}*$::')
 
 rm -f /usr/local/cyberpanel_upgrade.sh
-wget -O /usr/local/cyberpanel_upgrade.sh https://raw.githubusercontent.com/NNetworkBR/cyberpanel/$BRANCH_NAME/cyberpanel_upgrade.sh 2>/dev/null
+wget -O /usr/local/cyberpanel_upgrade.sh https://raw.githubusercontent.com/usmannasir/cyberpanel/$BRANCH_NAME/cyberpanel_upgrade.sh 2>/dev/null
 chmod 700 /usr/local/cyberpanel_upgrade.sh
 /usr/local/cyberpanel_upgrade.sh
