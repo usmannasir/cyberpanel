839b916256a4e37f7d842a9b0004fc87983ac5a5
Hasan Merkit <hasanmerkit@outlook.com>
2023-04-20 12:14:26 +0300

    Added PiluX v1 support

--- install.sh
+++ install.sh
@@ -16,26 +16,32 @@ elif echo $OUTPUT | grep -q "CloudLinux 7" ; then
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
                 SERVER_OS="CloudLinux"
+elif echo $OUTPUT | grep -q "AlmaLinux 8" ; then
+echo "Checking and installing curl and wget"
+yum install curl wget -y 1> /dev/null
+yum update curl wget ca-certificates -y 1> /dev/null
+                SERVER_OS="AlmaLinux"
+elif echo $OUTPUT | grep -q "Rocky Linux" ; then 
+echo "Checking and installing curl and wget"
+yum install curl wget -y 1> /dev/null
+yum update curl wget ca-certificates -y 1> /dev/null
+SERVER_OS="RockyLinux"
 elif echo $OUTPUT | grep -q "Ubuntu 18.04" ; then
 apt install -y -qq wget curl
                 SERVER_OS="Ubuntu"
 elif echo $OUTPUT | grep -q "Ubuntu 20.04" ; then
 apt install -y -qq wget curl
-                SERVER_OS="Ubuntu"
-elif echo $OUTPUT | grep -q "openEuler 20.03" ; then
-        echo -e "\nDetecting openEuler 20.03...\n"
-        SERVER_OS="openEuler"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
-elif echo $OUTPUT | grep -q "openEuler 22.03" ; then
-        echo -e "\nDetecting openEuler 22.03...\n"
-        SERVER_OS="openEuler"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
+                SERVER_OS="Ubuntu20"
+elif echo $OUTPUT | grep -q "Ubuntu 22.04" ; then
+apt install -y -qq wget curl
+                SERVER_OS="Ubuntu22"
+elif echo $OUTPUT | grep -q "PiluX" ; then
+apt install -y -qq wget curl
+                SERVER_OS="Ubuntu22"
 else
 
                 echo -e "\nUnable to detect your OS...\n"
-                echo -e "\nCyberPanel is supported on Ubuntu 18.04, CentOS 7.x and CloudLinux 7.x...\n"
+                echo -e "\nCyberPanel is supported on PiluX v1, Ubuntu 18.04, Ubuntu 20.04, Ubuntu 20.10, Ubuntu 22.04, CentOS 7.x, CentOS 8.x,  CloudLinux 7.x, CloudLinux 8.x and AlmaLinux 8.x...\n"
                 exit 1
 fi
 
