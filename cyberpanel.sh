67f7fedcd53a857d63b714807c62c87757b47308
usman@cyberpersons.com <usman@cyberpersons.com>
2023-04-20 23:10:15 +0500

    laid the foundation for arm64

--- cyberpanel.sh
+++ cyberpanel.sh
@@ -252,8 +252,8 @@ else
     exit
 fi
 
-if ! uname -m | grep -q x86_64 ; then
-  echo -e "x86_64 system is required...\n"
+if ! uname -m | grep -qE 'x86_64|aarch64' ; then
+  echo -e "x86_64 or ARM system is required...\n"
   exit
 fi
 
