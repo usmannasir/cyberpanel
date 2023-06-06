a717267d4f72f1cfc9d9b1aa971ce8fff542ae38
usman@cyberpersons.com <usman@cyberpersons.com>
2023-06-06 11:49:50 +0500

    update snappy version

--- plogical/test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-subprocess.run(self.command, capture_output=self.capture_output, text=True, shell=self.shell)
-if subprocess.run('uname -a', check=True, shell=True).find('arch64') ==-1:
-    pass
-else:
-    print('arch64')
