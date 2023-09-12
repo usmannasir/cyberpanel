a4528f39280b213c5a32c7ab2636218bd2093ab5
usmannasir <usman@cyberpersons.com>
2023-09-12 23:32:31 +0500

    add php 8.2 and also add support for cloud linux 8

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ elif echo $OUTPUT | grep -q "CentOS Linux 8" ; then
         SERVER_OS="CentOS8"
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
+elif echo $OUTPUT | grep -q "AlmaLinux 8" ; then
+        echo -e "\nDetecting AlmaLinux 8...\n"
+        SERVER_OS="CentOS8"
+yum install curl wget -y 1> /dev/null
+yum update curl wget ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "CloudLinux 7" ; then
         echo "Checking and installing curl and wget"
 yum install curl wget -y 1> /dev/null
@@ -40,7 +45,7 @@ yum update curl wget ca-certificates -y 1> /dev/null
 else
 
                 echo -e "\nUnable to detect your OS...\n"
-                echo -e "\nCyberPanel is supported on Ubuntu 18.04, CentOS 7.x and CloudLinux 7.x...\n"
+                echo -e "\nCyberPanel is supported on Ubuntu 18.04, Ubuntu 20.04 Ubuntu 22.04, AlmaLinux 8 and CloudLinux 7.x...\n"
                 exit 1
 fi
 
