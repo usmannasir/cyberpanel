fd5ffe5a95cb22ed23d39096717b4a7c3ae0b1c4
Gergely Lonyai <gergely@lonyai.com>
2023-11-21 13:48:29 +0100

    Update cyberpanel.sh

--- cyberpanel.sh
+++ cyberpanel.sh
@@ -839,6 +839,16 @@ if [[ $Server_OS = "CentOS" ]] ; then
       Check_Return "yum repo" "no_exit"
     yum install -y https://rpms.remirepo.net/enterprise/remi-release-9.rpm
       Check_Return "yum repo" "no_exit"
+    cat <<EOF >/etc/yum.repos.d/MariaDB.repo
+# MariaDB 10.4 CentOS repository list - created 2021-08-06 02:01 UTC
+# http://downloads.mariadb.org/mariadb/repositories/
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.11/rhel9-amd64/
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+enabled=1
+gpgcheck=1
+EOF
   fi
 
   if [[ "$Server_OS_Version" = "8" ]]; then
