24b10a3440438ac78ffbe68eb50986d96b1600ba
usmannasir <usman@cyberpersons.com>
2024-02-08 12:16:27 +0500

    bug fix: mariadb install

--- dockerManager/dockerInstall.py
+++ dockerManager/dockerInstall.py
@@ -23,7 +23,7 @@ class DockerInstall:
                 command = 'dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo'
                 ServerStatusUtil.executioner(command, statusFile)
 
-                command = 'sudo dnf install -y docker-ce --nobest'
+                command = 'sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin --nobest'
             elif ProcessUtilities.decideDistro() == ProcessUtilities.centos:
                 command = 'sudo yum install -y docker'
             else:
