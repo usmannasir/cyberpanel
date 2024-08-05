1d18d57f689b8a784182dbc10fbd4f957192dcb4
usmannasir <usman@cyberpersons.com>
2024-08-05 17:09:23 +0500

    remove acme python package

--- manageSSL/urls.py
+++ manageSSL/urls.py
@@ -4,16 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.loadSSLHome, name='loadSSLHome'),
 
-    path('manageSSL/', views.manageSSL, name='manageSSL'),
-    path('issueSSL/', views.issueSSL, name='issueSSL'),
+    path('manageSSL', views.manageSSL, name='manageSSL'),
+    path('issueSSL', views.issueSSL, name='issueSSL'),
 
-    path('sslForHostName/', views.sslForHostName, name='sslForHostName'),
-    path('obtainHostNameSSL/', views.obtainHostNameSSL, name='obtainHostNameSSL'),
+    path('sslForHostName', views.sslForHostName, name='sslForHostName'),
+    path('obtainHostNameSSL', views.obtainHostNameSSL, name='obtainHostNameSSL'),
 
-    path('sslForMailServer/', views.sslForMailServer, name='sslForMailServer'),
-    path('obtainMailServerSSL/', views.obtainMailServerSSL, name='obtainMailServerSSL'),
+    path('sslForMailServer', views.sslForMailServer, name='sslForMailServer'),
+    path('obtainMailServerSSL', views.obtainMailServerSSL, name='obtainMailServerSSL'),
 
     # v2 functions
-    path('v2ManageSSL/', views.v2ManageSSL, name='v2ManageSSL'),
-    path('v2IssueSSL/', views.v2IssueSSL, name='v2IssueSSL'),
+    path('v2ManageSSL', views.v2ManageSSL, name='v2ManageSSL'),
+    path('v2IssueSSL', views.v2IssueSSL, name='v2IssueSSL'),
 ]
