b668c3b092119e1df000dbc9c7baa7d3c7447571
usmannasir <usman@cyberpersons.com>
2024-08-07 01:29:20 +0500

    bug fix url in manage services

--- manageServices/urls.py
+++ manageServices/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('managePowerDNS/', views.managePowerDNS, name='managePowerDNS'),
-    path('managePostfix/', views.managePostfix, name='managePostfix'),
-    path('managePureFtpd/', views.managePureFtpd, name='managePureFtpd'),
+    path('managePowerDNS', views.managePowerDNS, name='managePowerDNS'),
+    path('managePostfix', views.managePostfix, name='managePostfix'),
+    path('managePureFtpd', views.managePureFtpd, name='managePureFtpd'),
 
-    path('fetchStatus/', views.fetchStatus, name='fetchStatus'),
-    path('saveStatus/', views.saveStatus, name='saveStatus'),
+    path('fetchStatus', views.fetchStatus, name='fetchStatus'),
+    path('saveStatus', views.saveStatus, name='saveStatus'),
 
-    path('manageApplications/', views.manageApplications, name='manageApplications'),
-    path('removeInstall/', views.removeInstall, name='removeInstall'),
+    path('manageApplications', views.manageApplications, name='manageApplications'),
+    path('removeInstall', views.removeInstall, name='removeInstall'),
 ]
