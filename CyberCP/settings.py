17dbcf95c5a9277cfb293fd2ebda9c15d526f77c
usmannasir <usman@cyberpersons.com>
2025-10-06 12:27:33 +0500

    Fix phpMyAdmin access issue for authenticated users
    - Removed session validation check from phpmyadminsignin.php
    - Removed PhpMyAdminAccessMiddleware from settings.py
    - These changes were preventing access to phpMyAdmin even for logged-in users
    - Reverts problematic authentication flow that was blocking legitimate access

--- CyberCP/settings.py
+++ CyberCP/settings.py
@@ -90,8 +90,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'CyberCP.secMiddleware.secMiddleware',
-    'CyberCP.phpmyadminMiddleware.PhpMyAdminAccessMiddleware'
+    'CyberCP.secMiddleware.secMiddleware'
 ]
 
 ROOT_URLCONF = 'CyberCP.urls'
