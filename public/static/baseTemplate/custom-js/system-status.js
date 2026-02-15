3cb149ba4f3b24379842ed9783660278c854134d
master3395 <master3395@users.noreply.github.com>
2026-02-15 01:16:01 +0100

    Version management: fix upgrade polling, branch dropdown, log visibility
    - Fix ReferenceError: use $timeout instead of timeout, store timer for cancel
    - Add v2.5.5-dev to Select Branch dropdown (remove dev filter)
    - Limit branch list to latest 10 by version
    - Fix Upgrade Progress Log text visibility (light text on dark bg)

--- public/static/baseTemplate/custom-js/system-status.js
+++ public/static/baseTemplate/custom-js/system-status.js
@@ -553,6 +553,7 @@ app.controller('versionManagment', function ($scope, $http, $timeout) {
     $scope.updateFinish = true;
     $scope.couldNotConnect = true;
 
+    var upgradeStatusTimer = null;
 
     $scope.upgrade = function () {
 
@@ -623,7 +624,8 @@ app.controller('versionManagment', function ($scope, $http, $timeout) {
             if (response.data.upgradeStatus === 1) {
 
                 if (response.data.finished === 1) {
-                    $timeout.cancel();
+                    if (upgradeStatusTimer) $timeout.cancel(upgradeStatusTimer);
+                    upgradeStatusTimer = null;
                     $scope.upgradelogBox = false;
                     $scope.upgradeLog = response.data.upgradeLog;
                     $scope.upgradeLoading = true;
@@ -635,7 +637,7 @@ app.controller('versionManagment', function ($scope, $http, $timeout) {
                 } else {
                     $scope.upgradelogBox = false;
                     $scope.upgradeLog = response.data.upgradeLog;
-                    $timeout(getUpgradeStatus, 2000);
+                    upgradeStatusTimer = $timeout(getUpgradeStatus, 2000);
                 }
             }
 
