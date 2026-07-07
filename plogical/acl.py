26b8bb8d04a15945d065e8d8bba5cebf0662fd66
Inertia-Squared <ojmiddlet@gmail.com>
2026-07-07 17:36:20 +1000

    fix(ACLManager) Match getPHPString based on pattern
    Amended to restore compatibility for Python versions 3.6-3.9
    
    Matches version string instead of manual list for PHPString.
    The regex pattern is future-proofed for different version formats, just in case :)
    
    Fixes #1726

--- plogical/acl.py
+++ plogical/acl.py
@@ -20,7 +20,7 @@ from subprocess import call, CalledProcessError
 from shlex import split
 from .CyberCPLogFileWriter import CyberCPLogFileWriter as logging
 from dockerManager.models import Containers
-from re import compile
+from re import compile, search
 
 class ACLManager:
 
@@ -617,37 +617,12 @@ class ACLManager:
 
     @staticmethod
     def getPHPString(phpVersion):
-
-        if phpVersion == "PHP 5.3":
-            php = "53"
-        elif phpVersion == "PHP 5.4":
-            php = "54"
-        elif phpVersion == "PHP 5.5":
-            php = "55"
-        elif phpVersion == "PHP 5.6":
-            php = "56"
-        elif phpVersion == "PHP 7.0":
-            php = "70"
-        elif phpVersion == "PHP 7.1":
-            php = "71"
-        elif phpVersion == "PHP 7.2":
-            php = "72"
-        elif phpVersion == "PHP 7.3":
-            php = "73"
-        elif phpVersion == "PHP 7.4":
-            php = "74"
-        elif phpVersion == "PHP 8.0":
-            php = "80"
-        elif phpVersion == "PHP 8.1":
-            php = "81"
-        elif phpVersion == "PHP 8.2":
-            php = "82"
-        elif phpVersion == "PHP 8.3":
-            php = "83"
-        elif phpVersion == "PHP 8.4":
-            php = "84"
-
-        return php
+        # Expected input: "PHP X.X"
+        result = search("(^|\\s)\\d+\\.\\d+", phpVersion)
+        if result:
+            php = result.group(0).replace(".", "")
+            return php
+        return None
 
     @staticmethod
     def searchWebsiteObjects(currentACL, userID, searchTerm):
