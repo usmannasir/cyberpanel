be2edc92269055c90ffc4fb54437afae7b336c03
CyberPanel <support@cyberpanel.net>
2026-08-10 04:54:48 +0530

    Keep Web Terminal enabled after upgrades

--- plogical/upgrade.py
+++ plogical/upgrade.py
@@ -5600,7 +5600,7 @@ pm.max_spare_servers = 3
                 0,
             )
 
-        command = 'systemctl restart fastapi_ssh_server'
+        command = 'systemctl enable --now fastapi_ssh_server'
         Upgrade.executioner(command, command, 0)
 
         Upgrade.stdOut("Upgrade Completed.")
